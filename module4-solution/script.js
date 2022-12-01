5b28df848cbb29672031880de77349a02b285d25
Tavin Mogodi <115457759+TavinMogodi@users.noreply.github.com>
2022-12-01 13:57:35 +0200

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -60,7 +60,7 @@ for (var i = 0; i < names.length; i++) {
   if (firstLetter === 'j') {
     byeSpeaker.speak("Goodbye " + names[i]);
   } else {
-    helloSpeaker.speak("Goodbye " + names[i]);
+    helloSpeaker.speak("Hello " + names[i]);
   }
 }
 
