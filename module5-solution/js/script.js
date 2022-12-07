b9512985d87834d889f1476c1418367a6b59840a
Tavin Mogodi <115457759+TavinMogodi@users.noreply.github.com>
2022-12-07 11:49:37 +0200

    Update script.js

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -102,7 +102,8 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      // var chosenCategoryShortName = ....
+      var chosenCategoryShortName = choosenRandomCategory (categories),short_name;
+      
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -116,7 +117,8 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      // var homeHtmlToInsertIntoMainPage = ....
+      var homeHtmlToInsertIntoMainPage = ....
+      
 
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
