10e6286d73a71263868af0d597442751526884a2
Tavin Mogodi <115457759+TavinMogodi@users.noreply.github.com>
2022-12-08 15:16:53 +0200

    Update script.js

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -85,7 +85,7 @@ $ajaxUtils.sendGetRequest(
  allCategoriesUrl,
  buildAndShowHomeHTML,// ***** <---- TODO: STEP 1: Substitute [...] ******
   true);// Explicitly setting the flag to get JSON from server processed into an object literal 
-},
+});
 // *** finish **
 
 
